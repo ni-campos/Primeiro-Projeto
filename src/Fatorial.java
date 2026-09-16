67ddff424aaf2e342cb6c7780d6a9d2f62bad7ce
Nicole Campos Alves <nicolecamposmedici@gmail.com>
2026-09-16 13:44:10 -0300

    Update Fatorial.java

--- src/Fatorial.java
+++ src/Fatorial.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Fatorial {
-}
+        public static void main(String[] args) {
+            Scanner leitura = new Scanner(System.in);
+
+            System.out.print("Digite um número: ");
+            int numero = leitura.nextInt();
+
+            int fatorial = 1;
+
+            for (int i = 1; i <= numero; i++) {
+                fatorial *= i;
+            }
+
+            System.out.println("O fatorial de " + numero + " é: " + fatorial);
+        }
+    }
