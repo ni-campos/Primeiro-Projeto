8d0231dc16b4b2242c531c358478209511e6fb59
Nicole Campos Alves <nicolecamposmedici@gmail.com>
2026-09-16 13:41:40 -0300

    Update Calculadora.java

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Calculadora {
+    static void main() {
+        Scanner leitura = new Scanner(System.in);
+        int escolha = 0;
+
+        while (escolha != 3) {
+            System.out.println("-----Menu-----");
+            System.out.println("1. Calcular área do quadrado");
+            System.out.println("2. Calcular área do círculo");
+            System.out.println("3. Sair");
+            System.out.print("Escolha uma opção: ");
+            escolha = leitura.nextInt();
+
+            if (escolha == 1) {
+                System.out.print("Digite o lado do quadrado: ");
+                double lado = leitura.nextDouble();
+                double areaQuadrado = lado * lado;
+                System.out.println("Área do quadrado: " + areaQuadrado);
+            } else if (escolha == 2) {
+                System.out.print("Digite o raio do círculo: ");
+                double raio = leitura.nextDouble();
+                double areaCirculo = 3.14 * raio * raio;
+                System.out.println("Área do círculo: " + areaCirculo);
+            } else if (escolha == 3) {
+                System.out.println("Programa encerrado.");
+            } else {
+                System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
 }
