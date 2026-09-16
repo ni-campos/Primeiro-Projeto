b91a4e2c3c2868928f25806af5bcda38116622b6
Nicole Campos Alves <nicolecamposmedici@gmail.com>
2026-09-16 13:45:25 -0300

    Update Tabuada.java

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Tabuada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Tabuada do " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
 }
