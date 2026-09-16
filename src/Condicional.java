c4ebd6a0c13dad75ba4c82f40fae12c08ea4c165
Nicole Campos Alves <nicolecamposmedici@gmail.com>
2026-09-16 13:43:12 -0300

    Update Condicional.java

--- src/Condicional.java
+++ src/Condicional.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Condicional {
+    static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int numeroGerado =0;
+
+        System.out.println("Digite um número:");
+        numeroGerado = leitor.nextInt();
+
+        if (numeroGerado % 2 == 0) {
+            System.out.println("Seu número é Par:");
+        }
+        else if (numeroGerado % 2 !=0 ){
+            System.out.println("Seu número é Ímpar");
+
+        }
+    }
 }
